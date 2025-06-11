4909dbe3ff629f77311afcf5c759bef42806187a
aekrylova <aekrylova@mdanderson.org>
2025-06-11 14:33:21 -0500

    Update 5b2_DEG_testing.R

--- workflow/scripts/5_APA_testing/5b2_DEG_testing.R
+++ workflow/scripts/5_APA_testing/5b2_DEG_testing.R
@@ -27,6 +27,9 @@ fprefix <- snakemake@params[["file_prefix"]]
 group_one <- snakemake@params[["group_one"]]
 group_two <- snakemake@params[["group_two"]]
 testing_column <- snakemake@params[["meta_testing_column"]]
+cutoff_pct <- snakemake@params[["cutoff_pct"]]
+min_cell_per_group <- snakemake@params[["min_cell_per_group"]]
+
 
 source(paste0(script_dir,'/scripts/5_APA_testing/scPASU_functions_for_differential_testing.R'))
 
@@ -43,7 +46,7 @@ meta[,testing_column] <- sub(" ", "_", meta[,testing_column])
 meta[,testing_column] <- sub("-", "_", meta[,testing_column])
 
 counts_by_cellstate <- stratify_matrix(merged_counts = merged_counts, meta = meta, vars=c(testing_column),
-                                     cutoff_pct = 0, min_cell_per_group = 0)
+                                     cutoff_pct = cutoff_pct, min_cell_per_group = min_cell_per_group)
 
 ###### group 1 v. group 2 testing ######
 
@@ -54,3 +57,9 @@ inputs <- create_test_inputs(test = 'DEG', all_groups = counts_by_cellstate, ide
 
 sig_genes <- DEG_DESeq2_pseudobulk(inputs = inputs, comp = c(group_one, group_two), test.used = 'LRT',
                                    padj_thres=0.01,l2fc_frac_thres=log2(1.5),outdir)
+
+# Print session info
+sessionInfo()
+cat('\n')
+
+cat('End of script ','\n')
